ec13bbb884dd6a265a240586931b253f94c6b9df
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-11-09 17:46:43 +0100

    feat: update to Swift 6.0

--- Package.swift
+++ Package.swift
@@ -1,61 +1,42 @@
-// swift-tools-version: 5.10
+// swift-tools-version:6.0
 
-///
 import PackageDescription
 
-
-///
 let package = Package(
     name: "ValueType-package",
     products: [
-        
-        ///
         .library(
             name: "ValueType-module",
             targets: ["ValueType-module"]
         ),
-        
-        ///
         .library(
             name: "ValueTypeTestToolkit",
             targets: ["ValueTypeTestToolkit"]
         ),
     ],
     dependencies: [
-        
-        ///
         .package(
             url: "https://github.com/jeremyabannister/ExpressionErgonomics",
-            "0.4.5" ..< "0.5.0"
+            .upToNextMinor(from: "0.4.6")
         ),
     ],
     targets: [
-        
-        ///
         .target(
             name: "ValueType-module",
             dependencies: [
                 "ExpressionErgonomics",
             ]
         ),
-        
-        ///
         .target(
             name: "ValueTypeTestToolkit",
             dependencies: [
-                
-                ///
                 .product(
                     name: "ExpressionErgonomicsTestToolkit",
                     package: "ExpressionErgonomics"
                 ),
-                
-                ///
                 "ValueType-module",
             ]
         ),
-        
-        ///
         .testTarget(
             name: "ValueType-module-tests",
             dependencies: [
@@ -63,8 +44,6 @@ let package = Package(
                 "ValueTypeTestToolkit",
             ]
         ),
-        
-        ///
         .testTarget(
             name: "ValueTypeTestToolkit-tests",
             dependencies: [
